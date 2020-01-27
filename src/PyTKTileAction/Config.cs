bde94e67d28099c8aea89f3ed5e0c960e19389eb
David <david@millionideas.de>
2020-01-27 03:44:22 +0100

    Added PyTKTileAction

---
+++ src/PyTKTileAction/Config.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace PyTKTileAction
+{
+    class Config
+    {
+        public int messageType { get; set; }
+
+        public Config()
+        {
+            messageType = 1;
+        }
+    }
+}
---
+++ src/PyTKTileAction/ModEntry.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using StardewValley;
+using StardewModdingAPI;
+using PyTK.Types;
+
+namespace PyTKTileAction
+{
+    public class ModEntry : Mod
+    {
+        internal Config config;
+        internal ITranslationHelper i18n => Helper.Translation;
+
+        public override void Entry(IModHelper helper)
+        {
+            string startingMessage = i18n.Get("template.start", new { mod = helper.ModRegistry.ModID, folder = helper.DirectoryPath });
+            Monitor.Log(startingMessage, LogLevel.Trace);
+
+            config = helper.ReadConfig<Config>();
+
+            Helper.Events.GameLoop.GameLaunched += GameLoop_GameLaunched;
+        }
+
+        private void GameLoop_GameLaunched(object sender, StardewModdingAPI.Events.GameLaunchedEventArgs e)
+        {
+            TileAction tileAction = new TileAction("alert", PyTKTileAction_Action);
+            tileAction.register();
+        }
+
+        private bool PyTKTileAction_Action(string key, string values, GameLocation location, Vector2 position, string layer)
+        {
+            HUDMessage hudMessage = new HUDMessage($"{values} (X{(int)position.X} Y{(int)position.Y})", config.messageType);
+            Game1.hudMessages.Add(hudMessage);
+
+            return true;
+        }
+    }
+}
